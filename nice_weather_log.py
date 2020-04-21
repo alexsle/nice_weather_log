1569b7780abe5a6dfb9df0f6f9b5af412906a5ef
Aleksandr Slepinin <61365918+alexsle@users.noreply.github.com>
2020-04-21 20:54:42 +0200

    Update nice_weather_log.py
    Deleted unused module line

--- nice_weather_log.py
+++ nice_weather_log.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time
-import xlsxwriter
 import openpyxl
 from openpyxl import Workbook
 from openpyxl import load_workbook
